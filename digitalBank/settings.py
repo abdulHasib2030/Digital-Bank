64ffdd815cc827b0f00b2bc381ca76f5e0ea1408
abdul <abdulhasib2030@gmail.com>
2023-10-14 12:34:32 +0600

    change database

--- digitalBank/settings.py
+++ digitalBank/settings.py
@@ -76,18 +76,24 @@ WSGI_APPLICATION = 'digitalBank.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'digitalbank',
-       'USER': 'abdul',
-       'PASSWORD': '12345678',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'digitalbank',
+#        'USER': 'abdul',
+#        'PASSWORD': '12345678',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+# }
 
 
 
